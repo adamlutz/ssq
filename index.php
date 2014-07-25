235725d213508f4d4ceaa0d47d93b0e9d9205fc2
Adam Lutz <amlutz160@gmail.com>
2014-07-24 19:31:55 -0500

    changed song order

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Sans Souci: the music.</title>
+    <title>Sans Souci</title>
     <META NAME="Description" CONTENT="Sans Souci: progressive string music.">
     <meta name="description" content="Sans Souci: progressive string music." />
     <meta name="keywords" content="bluegrass, string band, acoustic, folk, country, old crow medicine show" />
